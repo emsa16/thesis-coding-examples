a8ab70d9cf45a6e014b0f173952b123b06521c8b
Emil Sandberg <emil.hietanen@gmail.com>
2019-04-08 13:36:15 +0200

    Translated comments and updated deprecated syntax

--- sketch1.js
+++ sketch1.js
@@ -1,4 +1,4 @@
-// 引力・斥力モデル
+// Gravity / Repulsion model
 var num = 1000;
 var vx = new Array(num);
 var vy = new Array(num);
@@ -7,9 +7,9 @@ var y = new Array(num);
 var ax = new Array(num);
 var ay = new Array(num);
 
-var magnetism = 10.0; //引力の強さ マイナスにすると斥力になる。
-var radius = 1 ; //描画する円の半径
-var gensoku = 0.95; // 粒子の移動を減速させる
+var magnetism = 10.0; //Strength of attraction If you make it negative, it becomes a repulsive force.
+var radius = 1 ; //Radius of the circle to draw
+var gensoku = 0.95; // Slow down particle movement
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
@@ -35,23 +35,23 @@ function draw(){
   rect(0,0,width,height);
   
   for(var i=0; i<num; i++){
-    var distance = dist(touchX, touchY, x[i], y[i]); //dist(x1,y1,x2,y2) ２点間の距離を求める関数
-    //加速度は引力の中心からの距離の二乗に反比例する。
-    if(distance > 3){ //あまりマウスに近すぎる場合は加速度を更新しない
-      ax[i] = magnetism * (touchX - x[i]) / (distance * distance); 
-      ay[i] = magnetism * (touchY - y[i]) / (distance * distance);
+    var distance = dist(mouseX, mouseY, x[i], y[i]); //dist(x1,y1,x2,y2) Function to find the distance between two points
+    //Acceleration is inversely proportional to the square of the distance from the center of attraction.
+    if(distance > 3){ //Does not update the acceleration if it is too close to the mouse
+      ax[i] = magnetism * (mouseX - x[i]) / (distance * distance); 
+      ay[i] = magnetism * (mouseY - y[i]) / (distance * distance);
     }
-    vx[i] += ax[i]; // 1フレームあたりaxだけ速度vxを増加する。
-    vy[i] += ay[i]; // 1フレームあたりayだけ速度vyを増加する.
+    vx[i] += ax[i]; // Increase the speed vx by ax per frame.
+    vy[i] += ay[i]; // Increase speed vy by ay per frame.
     
     vx[i] = vx[i]*gensoku;
     vy[i] = vy[i]*gensoku;
     
-    x[i] += vx[i];  // 1フレームあたりvyピクセル進ませる。
-    y[i] += vy[i];  // 1フレームあたりvyピクセル進ませる。
+    x[i] += vx[i];  // Advance vx pixels per frame.
+    y[i] += vy[i];  // Advance vy pixels per frame.
     
-    var sokudo = dist(0,0,vx[i],vy[i]); // 速度のX,Y成分から速度を求める
-    var r = map(sokudo, 0, 5, 0, 255); //速度に応じた色を計算
+    var sokudo = dist(0,0,vx[i],vy[i]); // Find velocity from X and Y components of velocity
+    var r = map(sokudo, 0, 5, 0, 255); //Calculate color according to speed
     var g = map(sokudo, 0,5, 64, 255);
     var b = map(sokudo, 0,5, 128, 255);
     fill(r, g, b, 32);
